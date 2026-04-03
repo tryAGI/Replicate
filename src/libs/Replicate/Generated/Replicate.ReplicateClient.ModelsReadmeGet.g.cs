71b4f0c245c6ed62fd0608543b65611a02bc21fb
Konstantin S. <havendv@gmail.com>
2026-04-03 09:07:39 +0400

    feat: Updated OpenAPI spec (#182)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Replicate/Generated/Replicate.ReplicateClient.ModelsReadmeGet.g.cs
+++ src/libs/Replicate/Generated/Replicate.ReplicateClient.ModelsReadmeGet.g.cs
@@ -179,7 +179,6 @@ namespace Replicate
                 try
                 {
                     __response.EnsureSuccessStatusCode();
-
                     var __content = await __response.Content.ReadAsStringAsync(
 #if NET5_0_OR_GREATER
                         cancellationToken
